7b62e8b3d2cdd7fa7b69b5b1ef740d9d26c47977
Filipe Tenedini <105571583+FilipeTenedini@users.noreply.github.com>
2022-11-13 15:16:37 -0300

    Seconde deploy
    First page finalization;

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const cs = (el) => document.querySelectorAll(el);
 
 let cart = [];
 let letQt = 1;
+let position = ''
+
 
 window.addEventListener('load',() =>{
     c('.banner .banner-content h1').style.opacity = 1;
@@ -17,25 +19,28 @@ window.addEventListener('scroll', () => {
 
 
 everJson.map((item,index) => {
-    letQt = 1;
 
     let travelItem = c('.models .travel').cloneNode(true);
 
     travelItem.setAttribute('data-key', index) // 
     let indice = travelItem.getAttribute('data-key');
-    
 
     travelItem.querySelector('.travel-title').innerHTML = item.name;    
     travelItem.querySelector('.travel-desc').innerHTML = item.desc;
     travelItem.querySelectorAll('.travel-date')[0].innerHTML = item.date;
     travelItem.querySelectorAll('.travel-date')[1].innerHTML = item.finally;
-    travelItem.querySelector('.travel-price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    travelItem.querySelector('.travel-price').innerHTML = `R$ ${item.price.toLocaleString('pt-br')}`;
 
 
 
     // abrindo o MODAL 
-    travelItem.querySelector('.button').addEventListener('click', (e) => {
-        e.preventDefault();
+    travelItem.querySelector('.button').addEventListener('click', (e) => { 
+        e.preventDefault();        
+        c('aside').style.right = '-400px' //fechar o cart
+
+        position = indice;           
+        letQt = 1;
+
         c('.modal-img img').src = everJson[indice].img;
         c('.modal-reserve--title').innerHTML = everJson[indice].name;
         c('.description span').innerHTML = everJson[indice].desc;
@@ -57,7 +62,7 @@ everJson.map((item,index) => {
 });
 
 
-
+// modal QT
 c('.info-qt--mais').addEventListener('click', () => {
     letQt++;
     c('.info-qt').innerHTML = letQt;
@@ -71,19 +76,85 @@ c('.info-qt--menos').addEventListener('click', () => {
     }
 });
 
+
 function closeModal(){
     c('.travel--modal-area').style.opacity = 0;
     setTimeout(()=>{ 
         c('.travel--modal-area').style.display = 'none';
         }, 300);
-} 
+} ;
 
-c('.travel--modal-area').addEventListener('click', () => {
-    if ( c('.travel--modal-area').classList.contains('info-qt') ) {
-        
-    }
+
+cs('.modal-reserve .button')[0].addEventListener('click', () => {
+    closeModal();
 });
 
-c('.modal-reserve .button').addEventListener('click', () => {
+
+cs('.modal-reserve .button')[1].addEventListener('click', () => {    
+
+    let localItem = cart.findIndex((item) => item.id == everJson[position].id)
+    if (localItem > -1) {
+        everJson[position].qt += letQt;
+    } else{
+        everJson[position].qt = letQt;
+        cart.push(everJson[position]) 
+    }
+    updateCart();
     closeModal();
+});
+
+
+
+function updateCart() {
+    let subtotal = 0;
+    let desconto = 0
+    let total = 0;
+
+    if (cart.length > 0 ){
+        c('aside').style.right = '0px'
+        c('.travel--modal-area').style.margin = 'auto'
+        c('.cart--reserve-items').innerHTML = '';
+
+        for (let i in cart){
+            let itemCart = c('.models .cart-item--area').cloneNode(true);
+            let choicedItem = everJson.find((item) => item.id == cart[i].id )
+            
+            subtotal += choicedItem.price * choicedItem.qt;
+    
+            itemCart.querySelector('.cart-item--img img').src = choicedItem.img
+            itemCart.querySelector('.cart-item--nome').innerHTML = choicedItem.name
+            itemCart.querySelector('.cart-item--preco').innerHTML = `R$ ${choicedItem.price.toLocaleString('pt-br')}`
+            itemCart.querySelector('.cart-qt').innerHTML = choicedItem.qt;
+                        
+            itemCart.querySelector('.cart-qtmenos').addEventListener('click', () => {
+                if (choicedItem.qt > 1){
+                    choicedItem.qt--;
+                } else {
+                    cart.pop(choicedItem);
+                }
+                updateCart();
+
+            });
+            itemCart.querySelector('.cart-qtmais').addEventListener('click', () => {
+                choicedItem.qt++;
+                updateCart();
+            });
+
+            c('.cart--reserve-items').append(itemCart) 
+        }
+        
+
+        desconto = subtotal * 0.10;
+        total = subtotal - desconto;
+        c('.subtotal-price').innerHTML = `R$ ${subtotal.toLocaleString('pt-br')}`;
+        c('.desc-price').innerHTML = `R$ ${desconto.toLocaleString('pt-br')}`;
+        c('.total-price').innerHTML = `R$ ${total.toLocaleString('pt-br')}`;
+    } else {
+        c('aside').style.right = '-400px'
+    }
+
+};
+
+c('.cart button').addEventListener('click', () => {
+    document.location.reload(true)
 });
--- tralves.js
+++ tralves.js
@@ -1,6 +1,6 @@
 let everJson = [
-    {id:1, name:'BASECAMP', desc:"If you don't have much experience, you can also visit Everest and get to Basecamp", date:'23/11/2022', finally:'30/11/2022', price: 3750.00, img: './assets/images/basecamp.jpg'},
-    {id:2, name:'NV TWO', desc:"Do you want to venture a little beyond basecamp and live an inexplicable experience?", date:'23/11/2022', finally:'1/12/2022', price: 4900.00, img: './assets/images/camp2.jpg'},
-    {id:3, name:'NV THREE', desc:"The third level is already a great victory! The view from this point is unforgettable.", date:'01/01/2023', finally:'13/12/2022', price: 7600.00, img: './assets/images/camp3.jpg'},
-    {id:4, name:'TOP', desc:"Advanced level camp only for the most resilient and experienced people.", date:'03/03/2023',finally:'02/04/2023', price: 16500.00, img: './assets/images/camp4.jpg'}
+    {id:1, name:'BASECAMP', desc:"If you don't have much experience, you can also visit Everest and get to Basecamp", date:'23/11/2022', finally:'30/11/2022', price: 3749.99, img: './assets/images/basecamp.jpg'},
+    {id:2, name:'NV TWO', desc:"Do you want to venture a little beyond basecamp and live an inexplicable experience?", date:'23/11/2022', finally:'1/12/2022', price: 4899.99, img: './assets/images/camp2.jpg'},
+    {id:3, name:'NV THREE', desc:"The third level is already a great victory! The view from this point is unforgettable.", date:'01/01/2023', finally:'13/12/2022', price: 7587.99, img: './assets/images/camp3.jpg'},
+    {id:4, name:'TOP', desc:"Advanced level camp only for the most resilient and experienced people.", date:'03/03/2023',finally:'02/04/2023', price: 16387.99, img: './assets/images/camp4.jpg'}
 ];
